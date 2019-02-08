187e9fc0f88d9667e010f7a3ef258dc48e15eef9
Ian Lintner <ian.lintner@workiva.com>
2019-02-08 16:25:28 -0600

    Fix for readline lib upgrades.

---
+++ readlinefix.bash
@@ -0,0 +1,5 @@
+# https://gist.github.com/wbotelhos/46c37807c834ccb5bb406e426adfe347
+ln -s /usr/local/opt/readline/lib/libreadline.8.0.dylib /usr/local/opt/readline/lib/libreadline.7.0.dylib
+ln -s /usr/local/opt/readline/lib/libreadline.8.0.dylib /usr/local/opt/readline/lib/libreadline.7.dylib
+ln -s /usr/local/opt/readline/lib/libreadline.8.0.dylib /usr/local/opt/readline/lib/libreadline.6.0.dylib
+ln -s /usr/local/opt/readline/lib/libreadline.8.0.dylib /usr/local/opt/readline/lib/libreadline.6.dylib
