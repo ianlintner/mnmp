959a66cb55117bc1a40e6da7ab0a2e2c33352d09
Ian Lintner <lintner.ian@gmail.com>
2019-02-25 14:44:09 -0600

    Create yeti.sh

---
+++ yeti.sh
@@ -0,0 +1,29 @@
+# You found the YETI. 
+# Hope you know what you are doing
+
+echo 'HACKING TEH GIBSON...'
+ln -S /usr/local/etc ~/etc
+echo '.......... etc'
+ln -S /usr/local/var ~/var
+echo '..... var'
+ln -S $(phpbrew path etc) /usr/local/etc/phpbrew
+echo '................... P H P B R E W ........'
+
+cat <<"EOF"
+----------Oooo---
+-----------(----)---
+------------)--/----
+------------(_/-
+----oooO----
+----(---)----
+-----\--(--
+------\_)-
+-----------Oooo---
+-----------(----)---
+------------)--/----
+------------(_/-
+----oooO----
+----(---)----
+-----\--(--
+------\_)- 
+EOF
